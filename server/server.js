0384d2619e2f65848cbcff2304a2bbb4c7bf3795
{mayank} <{mayankguptamg.11@gmail.com}>
2018-12-09 00:50:02 +0530

    socket interaction

--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
 const path = require('path');
-console.log(__dirname + '/../public')
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 var app = express();
+
 const publicPath = path.join(__dirname + '/../public');
+var server = http.createServer(app);
 var port = process.env.PORT || 3000;
-console.log(publicPath);
+
+var io = socketIO(server);
+console.log("io is ::", io);
 
 app.use(express.static(publicPath));
 app.post('/index.html', () => {
     console.log(publicPath + '/index.html')
 });
 
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+
+    console.log(`New user is connected`);
+
+
+    socket.on('disconnect', () => {
+        console.log("User is disconnected from server");
+    })
+
+});
+
+server.listen(port, () => {
     console.log(publicPath + '/index.html')
-    console.log(`Server is up at ${port}`);
+
 
 })
